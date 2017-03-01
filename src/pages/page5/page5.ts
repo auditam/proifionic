d2f6ac1e80ca2fd7a6649a64bc77d00adfb69f04
Auditama <westcoastcllaperz@yahoo.com>
2017-03-01 11:30:33 +0700

    rubah ke v2 masih step1

---
+++ src/pages/page5/page5.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-page5',
+  templateUrl: 'page5.html'
+})
+export class Page5 {
+
+  constructor(public navCtrl: NavController) {
+    
+  }
+
+}
